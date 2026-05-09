218ab04ee4710872c439264a536d93730d93a002
fsm-works <fsm.works2026@gmail.com>
2026-05-10 01:10:44 +0900

    fix: google.colab の不要なインポートを削除

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ import io
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
-from google.colab import files
 import googleapiclient.discovery
 from googleapiclient.http import MediaFileUpload
 
